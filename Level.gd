1ba90ab2e51bd0ab5194dfc0d0a7cd5eb9d7ecd3
OliveADay <sharinggon23@gmail.com>
2024-02-05 14:58:21 -0800

    ch

---
+++ Level.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+@export var totalGold = 0
+
+var remainingGold = 0
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	 # Replace with function body.
+	remainingGold = totalGold
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
