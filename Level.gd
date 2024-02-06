72f7bb8d1b808da44061c9d55d0a146d4f2370df
OliveADay <sharinggon23@gmail.com>
2024-02-05 21:58:31 -0800

    winstate

--- Level.gd
+++ Level.gd
@@ -1,13 +1,17 @@
 extends Node2D
 
-@export var totalGold = 0
+@export var totalGold = 1
+@export var winUI = Control
 
 var remainingGold = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	 # Replace with function body.
 	remainingGold = totalGold
+	print(remainingGold)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if(remainingGold == 0):
+		winUI.visible= true;
--- gold.gd
+++ gold.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+@export var Level = Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,4 +11,5 @@ func _ready():
 func _process(delta):
 	if(get_overlapping_bodies()):
 		visible = false
+		Level.remainingGold -= 1
 		set_process(false)
