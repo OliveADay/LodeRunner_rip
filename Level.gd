336247a8b80d97a00044e2855f912f73ec35998a
OliveADay <sharinggon23@gmail.com>
2024-02-09 09:50:59 -0800

    bigchanges of nothing

--- Level.gd
+++ Level.gd
@@ -2,15 +2,19 @@ extends Node2D
 
 @export var totalGold = 1
 @export var winUI = Control
+@export var winTimer = Timer
+ 
 
 var remainingGold = 0
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	 # Replace with function body.
 	remainingGold = totalGold
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if(remainingGold == 0):
-		winUI.visible= true;
+		winUI.visible = true
+
+
+func _on_timer_timeout():
+	print("hehehehehehehe") # Replace with function body.
---
+++ World.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+var L2 = preload("res://l_2.tscn").instantiate()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#get_tree().root.add_child(L2)
+	#get_tree().root.remove_child($L1) 
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
