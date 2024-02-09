010732ad18424e8cf8d87d95bece0bc745d1565d
OliveADay <sharinggon23@gmail.com>
2024-02-09 11:03:44 -0800

    s

--- Level.gd
+++ Level.gd
@@ -3,18 +3,25 @@ extends Node2D
 @export var totalGold = 1
 @export var winUI = Control
 @export var winTimer = Timer
- 
+@export var whatLev = 1
+
+var world = [Node]
+
 
 var remainingGold = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	 # Replace with function body.
 	remainingGold = totalGold
+	remove_child($ColorRect2)
+	world = get_tree().get_nodes_in_group("World")
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	world = get_tree().get_nodes_in_group("World")
 	if(remainingGold == 0):
+		add_child(winUI)
 		winUI.visible = true
-
+		
 
 func _on_timer_timeout():
-	print("hehehehehehehe") # Replace with function body.
+	world[0]._Trans_1_2(whatLev) # Replace with function body.
--- World.gd
+++ World.gd
@@ -1,14 +1,20 @@
 extends Node2D
 
 var L2 = preload("res://l_2.tscn").instantiate()
-
+var L3 = preload("res://L3.tscn").instantiate()
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#get_tree().root.add_child(L2)
-	#get_tree().root.remove_child($L1) 
 	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	
+func _Trans_1_2(lev):
+	if(lev == 1):
+		add_child(L2)
+		$L1.queue_free()
+	if(lev == 2):
+		add_child(L3)
+		$L2.queue_free()
