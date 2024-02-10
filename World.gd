8fc3e159e4ad5f4c716b6c6b9eade69668fc427c
OliveADay <sharinggon23@gmail.com>
2024-02-10 11:55:01 -0800

    s

--- World.gd
+++ World.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 var L2 = preload("res://l_2.tscn").instantiate()
 var L3 = preload("res://L3.tscn").instantiate()
+var L4 = preload("res://L4.tscn").instantiate()
+var L5 = preload("res://L5.tscn").instantiate()
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
@@ -19,3 +21,9 @@ func _Trans_1_2(lev):
 	if(lev == 2):
 		add_child(L3)
 		$L2.queue_free()
+	if(lev == 3):
+		add_child(L4)
+		$L3.queue_free()
+	if(lev == 4):
+		add_child(L5)
+		$L4.queue_free()
